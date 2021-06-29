720dbbd96d924676ec23cc8b9e2ce80984919ce7
webdev1991 <rajesh.baral91@gmail.com>
2021-06-29 18:15:27 +0530

    modified config database and routes & adding news file & madel

--- application/config/database.php
+++ application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] = array(
 	'database' => 'codeigniter3',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => FALSE,
+	'pconnect' => TRUE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
