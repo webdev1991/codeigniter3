50503b119bbfac6588fde4735af5000cb64e5913
webdev1991 <rajesh.baral91@gmail.com>
2021-07-01 08:11:23 +0530

    update information

---
+++ application/views/news/create.php
@@ -0,0 +1,23 @@
+<h2><?php echo $title; ?></h2>
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('news/create'); ?>
+
+    <h5>Username</h5>
+<input type="text" name="username" value="" size="50" />
+
+<h5>Password</h5>
+<input type="text" name="password" value="" size="50" />
+
+<h5>Password Confirm</h5>
+<input type="text" name="passconf" value="" size="50" />
+
+<h5>Email Address</h5>
+<input type="text" name="email" value="" size="50" />
+
+<div><input type="submit" value="Submit" /></div>
+
+</form>
+
+</form>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,37 @@
 <html>
-        <head>
-                <title>CodeIgniter Tutorial</title>
-        </head>
-        <body>
+<head>
+        <title>CodeIgniter Tutorial</title>
+        <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">CodeIgniter3</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="<?php echo base_url(); ?>">Home</a></li>
+      <li class="active"><a href="<?php echo base_url(); ?>about">About</a></li>
+
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Contact Us <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Submit Contact</a></li>
+          <li><a href="#">View Contacts</a></li>
+        </ul>
+
+        <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Blog <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="<?php echo base_url(); ?>news/create">Blog Post</a></li>
+          <li><a href="<?php echo base_url(); ?>news">View Blog</a></li>
+        </ul>
+
+    </ul>
+  </div>
+</nav>
 
            <h1><?php echo $title; ?></h1>
