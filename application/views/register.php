07be298dab7d91f8638f01e01b714988b30e10cc
webdev1991 <rajesh.baral91@gmail.com>
2021-07-03 18:07:03 +0530

    update register file

--- application/views/register.php
+++ application/views/register.php
@@ -3,11 +3,6 @@
 <head>
 	<meta charset="utf-8">
 	<title>Register</title>
-
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap.min.css'?>">
-	<script type="text/javascript" src="<?php echo base_url().'jquery/jquery.min.3.6.0.js'?>"></script>
-	<script type="text/javascript" src="<?php echo base_url().'assets/js/bootstrap.bundle.min.js'?>"></script>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/registercss/style.css'?>">
 </head>
 <body>
 
